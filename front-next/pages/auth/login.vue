a53c35d7e2732a8735b5caacb77ed5cab6215a79
Your Name <you@example.com>
2025-04-12 11:14:16 +0000

    singlecontainer ok

---
+++ front-next/pages/auth/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <ContainerSingle>
+    <template #title>Log in to your Account</template>
+    <template #main> Main </template>
+    <template #footer>
+      <UButton to="/auth/send-reset-password-link" label="Forgot your password?" variant="ghost" />
+    </template>
+  </ContainerSingle>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "solo",
+})
+</script>
