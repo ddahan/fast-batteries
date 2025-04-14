d2d154055f0165f61ebc00d0ba2be4594fd54db3
Your Name <you@example.com>
2025-04-14 13:29:22 +0000

    Fix username vs login field with oAuth2

--- front-next/components/auth/form/Login.vue
+++ front-next/components/auth/form/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <UForm ref="form" :state="state" :schema="schema" @submit="onSubmit" novalidate>
     <ContainerVerticalInputs>
-      <FormGroupEmail v-model="state.username" :inputAttrs="{ autofocus: true, class: 'w-full' }" />
+      <FormGroupEmail v-model="state.email" :inputAttrs="{ autofocus: true, class: 'w-full' }" />
       <FormGroupPassword
         v-model="state.password"
         :inputAttrs="{ autocomplete: 'current-password', class: 'w-full' }"
@@ -20,12 +20,12 @@ const form = useTemplateRef("form")
 const status: Ref<RequestStatus> = ref("idle")
 
 const schema = z.object({
-  username: z.string().email("Invalid email"),
-  password: z.string(),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(1, "Password cannot be empty"),
 })
 
 const state = reactive<Schema>({
-  username: "",
+  email: "",
   password: "",
 })
 
@@ -34,9 +34,11 @@ type Schema = z.output<typeof schema>
 const onSubmit = async (event: FormSubmitEvent<Schema>) => {
   const token = await myFetch(form, status)<Token>("auth/access-token", {
     method: "post",
-    body: new URLSearchParams(state), // required format for OAuth2
+    body: new URLSearchParams({
+      username: state.email, // replace "email" with "username" here for OAuth2 compliance
+      password: state.password,
+    }),
   })
-  console.log(token)
   if (token) {
     login(token)
   }
